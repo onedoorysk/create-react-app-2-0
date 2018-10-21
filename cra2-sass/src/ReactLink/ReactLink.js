df3af04ddbaaa938814a41afc1577a7e3131e80e
onedoorysk <rysk1008@gmail.com>
2018-10-21 18:08:27 +0900

    study sass-support

---
+++ cra2-sass/src/ReactLink/ReactLink.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './ReactLink.scss'
+
+export default props => {
+  const { className, children, ...rest } = props
+  return (
+    <a className={ `ReactLink ${className}` } {...rest} >
+      {children}
+    </a>
+  )
+}
